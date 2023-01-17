04965fe2db53e97ab7c03c33f9a23e9d34d19de2
Mostafa Ahangarha <ahangarha@riseup.net>
2023-01-17 23:35:46 +0330

    WIP - test expose-loader

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -3,3 +3,9 @@
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
+console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+console.log(componentRequireContext)
+console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+
+throw 'We are in app/javascript/packs/server-rendering.js'
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,16 @@
 const { webpackConfig } = require('shakapacker')
 
+
+// throw "in webpack.config"
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+
+const exposeReactRailsUJS = {
+  test: require.resolve('react_ujs'),
+  use: [{ loader: 'expose-loader', options: { exposes: ['$', 'ReactRailsUJS'] } }],
+};
+
+webpackConfig.module.rules.push(exposeReactRailsUJS);
+
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 
 module.exports = webpackConfig
