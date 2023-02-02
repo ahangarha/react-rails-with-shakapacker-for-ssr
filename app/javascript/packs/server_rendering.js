2b556aee6f0692d44269f6b7b6f0e4803511de1d
Judah Meek <judah.meek@gmail.com>
2023-02-02 01:54:14 -0600

    whatever

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,11 +1,5 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
 var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
+require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-
-console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-console.log(componentRequireContext)
-console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-
-throw 'We are in app/javascript/packs/server-rendering.js'
